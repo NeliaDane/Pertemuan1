c6b97ba6f31c0522e2fdce031710cb1ebe9b6305
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-10 14:43:37 +0700

    1. contoh program pertama 2. contoh program literal

---
+++ src/main/kotlin/2_Literal.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+//    mendeklarasi literal
+
+//    literal pada tipe data int/integer/angka
+    var intLiteral = 5
+
+//    literal pada tipe data double/float (bilangan berkoma)
+    var doubleLiteral = .02
+
+//    literal pada tipe data string
+    var stringLiteral = "Hello"
+
+//    literal pada tipe data char/character
+    var charLiteral = '1'
+
+//    literal pada tipe data boolean
+    var boolLiteral = true
+
+//    mencetak nilai sesuai tipe data yang sudah di tentukan nilainya
+    println("intLiteral = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringLiteral = $stringLiteral")
+    println("charLiteral = $charLiteral")
+    println("boolLiteral = $boolLiteral")
+}
