2a4cd24116d2b079ebb9296d86659be781b5662a
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-10 14:52:20 +0700

    3. blok dan komentar

---
+++ src/main/kotlin/3_Blok.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Hello, ini program pertama saya di kotlin!")
+}
+
+//blok di tandai dengan buka dan tutup kurung kurawal. Blok di gunakan untuk menulis ikatan sebuah statement yang di kelompokkan bersama menjadi satu bagian.
+
+//komentar
+//tanda ini hanya bisa membuat 1 baris komentar saja
+/*
+tanda atau simbol ini di gunakan untuk komentar multi baris atau banyak barisnya.
+
+ */
+
+/**
+ * jenis komentar ini di gunakan untuk mendokumentasikan baris kode program
+ */
