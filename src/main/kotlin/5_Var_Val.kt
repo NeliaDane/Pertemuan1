bfe24f1f649ac1877626db9e6551f3937253e514
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-10 15:14:04 +0700

    5. var dan val

---
+++ src/main/kotlin/5_Var_Val.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    /*
+    var berfungsi untuk mendeklarasikan variabel yang nilainya dapat berubah
+    selama program di jalankan atau di eksekusi.
+     */
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+
+    /*
+    val berfungsi untuk deklarasi variabel konstanta yang mempunyai nilai tetap
+    atau nilainya tidak bisa dirubah.
+     */
+    val phi = 3.14
+}
