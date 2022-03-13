bb05993eb44a75b73c393402b2cddf0704292a4b
[Nelia Dane] <neliadane17@gmail.com>
2022-03-13 13:47:33 +0700

    13. contoh boolean

---
+++ src/main/kotlin/13_Boolean.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+//    boolean memiliki nilai true dan false. Nilai 0 dan 1 tidak berlaku untuk menunjukkan nilai pada sebuah variabel bertipe boolean.
+    var varBool : Boolean = true
+    if(varBool)
+        println("varBool bernilai " + varBool)
+
+    /*
+    var varInt : Int = 1
+    if(varInt)
+        println("varInt bernilai " + varInt)
+
+        jika di tambahkan kode program di atas maka akan terjadi error
+        karna tipe integer yang di simpulkan pada tipe boolean.
+     */
+}
