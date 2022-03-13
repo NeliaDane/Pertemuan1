8aa583ce2ca7e542fc5ce34cbe81811ddb1ae0e3
[Nelia Dane] <neliadane17@gmail.com>
2022-03-13 11:54:01 +0700

    7. contoh dari constant literal

---
+++ src/main/kotlin/7_Constant_Literal.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+
+//    contoh literal float (bilangan pecahan). Float di tandai dengan F (f kapital)
+    val floatLiteral : Float = 178.95F
+//    mencetak nilai konstanta atau nilai yang sudah di tetapkan pada literal float di atas.
+    println("Contoh Literal Float : " + floatLiteral)
+
+//    contoh literal long. long di tandai dengan L (l kapital)
+    val oneBillion: Long = 1000000000L
+//    mencetak nilai konstanta atau nilai yang sudah di tetapkan pada literal long
+    println("Contoh Literal Long : " + oneBillion)
+
+//    contoh literal heksadesimal.Literal heksadesimal diawali dengan 0x.
+    val valHeksa = 0x0F
+//    mencetak nilai yang sudah di tetapkan pada literal heksadesimal di atas.
+    println("Contoh Literal Heksadesimal : " + valHeksa)
+
+//    contoh literal binary diawali dengan 0b
+    val valBinary = 0b00001011
+//    mencetak nilai yang sudah di tetapkan pada literal binary di atas
+    println("Contoh Literal Binary : " + valBinary)
+
+//    contoh literal konvensional floating point number
+    val notasiKonvensional = 123.5e10
+    println("Contoh Literal Konvensional Floating Point Number : " + notasiKonvensional)
+}
